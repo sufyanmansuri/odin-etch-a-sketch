c8d9805a46720ed03b75b08066890c0c3ded6f5d
Sufyan Mansuri <sufyan8834@gmail.com>
2022-07-21 22:37:35 +0530

    Draw pixel on mouse hover

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-function drawSquares() {
-  const pad = document.querySelector(".pad");
-  for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 16; j++) {
-      const box = document.createElement("div");
-      box.classList.add("box");
-      row.append(box);
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    const pad = document.querySelector(".pad");
+    for (let i = 0; i < 16; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      for (let j = 0; j < 16; j++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        row.append(box);
+      }
+      pad.append(row);
     }
-    pad.append(row);
-  }
+  },
+  false
+);
+
+function afterPageLoad() {
+  window.boxes = document.querySelectorAll(".box");
+  window.boxes.forEach((box) => {
+    box.addEventListener("mouseover", drawPixel);
+  });
+}
+
+function drawPixel(e) {
+  e.target.style.backgroundColor = "black"
 }
